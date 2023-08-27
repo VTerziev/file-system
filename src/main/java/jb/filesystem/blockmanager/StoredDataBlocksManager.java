71d595944a9673aa547b292b56979ac57c4c42fa
VTerziev <viktortrz@gmail.com>
2023-08-28 02:00:44 +0300

    Check for blocks availability in DataStorage

--- src/main/java/jb/filesystem/blockmanager/StoredDataBlocksManager.java
+++ src/main/java/jb/filesystem/blockmanager/StoredDataBlocksManager.java
@@ -16,6 +16,9 @@ public class StoredDataBlocksManager implements DataBlocksManager {
 
     @Override
     public DataBlock getBlock(int blockId) {
+        if (!bitmask.isAvailable(blockId)) {
+            throw new IllegalArgumentException("Block " + blockId + " is not allocated");
+        }
         DataBlock[] buffer = new DataBlock[1];
         dataStorage.read(blockId, 1, buffer);
         return buffer[0];
@@ -35,6 +38,9 @@ public class StoredDataBlocksManager implements DataBlocksManager {
 
     @Override
     public void deallocateBlock(int blockId) {
+        if (!bitmask.isAvailable(blockId)) {
+            throw new IllegalArgumentException("Block " + blockId + " is not allocated");
+        }
         System.out.println("Deallocating data block: " + blockId);
         bitmask.deallocateBlock(blockId);
     }
